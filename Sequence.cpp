56acd1ebb8f1b00c1ad80d2f5a85b07ed652d382
RolaChen <976950078@qq.com>
2018-06-23 11:26:52 +0800

    finish longestConsecutive()

--- Sequence.cpp
+++ Sequence.cpp
@@ -33,8 +33,53 @@ int Sequence::numberOf(char base)
 
 string Sequence::longestConsecutive()
 {
-  string a;
-  return a;
+  bool start=1;
+  bool who=1; //who=1 represent a1; who=0 represent a2
+  string a1,a2;
+  int leng=dna.length();
+  for(int i=0;i<leng;i++)
+  {
+     if(start)
+       a1=dna[i],start=0;
+     else
+     {
+       if(who)
+       {
+         if(a1[0]==dna[i])
+           a1+=dna[i];
+         else
+         {
+           if(a1.length()>a2.length())
+             who=0,a2=dna[i];
+           else
+             a1=dna[i];
+         }
+       }
+       else
+       {
+         if(a2[0]==dna[i])
+           a2+=dna[i];
+         else
+         {
+           if(a2.length()>a1.length())
+             who=1,a1=dna[i];
+           else
+             a2=dna[i];
+         }
+       }   
+     }
+  }
+  if(a1.length()>a2.length())
+    return a1;
+  else if(a1.length()<a2.length()) 
+    return a2;
+  else
+  {
+    if(who)
+      return a1;
+    else
+      return a2;
+   }
 }
 
 string Sequence::longestRepeated()
